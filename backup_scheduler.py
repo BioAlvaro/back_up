4067564af9a2d4816d8dfe1a79f449cba24ed966
Alvaro Sanchez <71273913+svalvaro@users.noreply.github.com>
2021-06-25 13:12:00 +0200

    Add files via upload
    python scheduler

---
+++ backup_scheduler.py
@@ -0,0 +1,18 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import date, datetime
+import os
+import hashlib
+
+
+def start_backup():
+    date_now = datetime.now()
+    date_now = date_now.strftime("%d/%m/%Y %H:%M:%S")
+
+    print('Back up started on: ', date_now)
+
+    exec(open('./backup_v2.py').read()) # Read and execute the back up scrip
+    
+
+scheduler = BlockingScheduler()
+scheduler.add_job(start_backup, 'cron', year = '*', month = '*', day = '*', hour= '13', minute = '11', second = '20')
+scheduler.start()
